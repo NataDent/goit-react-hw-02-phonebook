a9f94de7e74f69d691ba9e57cfb43ed5754f71a7
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-09-03 18:44:16 +0300

    Contactform is completed

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,52 @@
+import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
+import {
+  FormWrapper,
+  FormStyled,
+  Label,
+  FieldWrapper,
+  ErrorMessageStyled,
+  AddButton,
+} from './ContactForm.styled';
+
+const contactSchema = Yup.object().shape({
+  name: Yup.string().min(2, 'Too short!').required('Required'),
+  number: Yup.string()
+    .min(7, 'Too Short!')
+    .max(10, 'Too long!')
+    .required('Required'),
+});
+
+export const ContactForm = ({ onAdd }) => {
+  return (
+    <FormWrapper>
+      <Formik
+        initialValues={{
+          name: '',
+          number: '',
+        }}
+        validationSchema={contactSchema}
+        onSubmit={(values, actions) => {
+          onAdd(values);
+          actions.resetForm();
+        }}
+      >
+        <FormStyled>
+          <FieldWrapper>
+            <Label htmlFor="name">Name</Label>
+            <Field id="name" name="name" placeholder="Jane" />
+            <ErrorMessageStyled component="div" name="name" />
+          </FieldWrapper>
+
+          <FieldWrapper>
+            <Label htmlFor="number">Number</Label>
+            <Field id="number" name="number" placeholder="123-45-67" />
+            <ErrorMessageStyled component="div" name="number" />
+          </FieldWrapper>
+
+          <AddButton type="submit">Submit</AddButton>
+        </FormStyled>
+      </Formik>
+    </FormWrapper>
+  );
+};
---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const SearchWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-top: 20px;
+  gap: 8 px;
+`;
+
+export const SearchInput = styled.input`
+  margin-top: 10px;
+  padding: 10px;
+  font-size: 18px;
+  color: #000;
+  outline: none;
+  border: 1px solid black;
+  border-radius: 8px;
+  background-color: #fff;
+`;
